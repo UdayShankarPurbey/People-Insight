689776346cf46fafd24a0b10cc7355b119a1944c
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-20 10:34:08 +0530

    add home page

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
+    NgbCarouselModule
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
