b880d1aa82d19138597d2db48e4a8f523294c048
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-20 10:57:18 +0530

    added Gallery Component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 import { HomeComponent } from './pages/home/home.component';
+import { GalleryComponent } from './pages/gallery/gallery.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path : 'home', component : HomeComponent},
     { path: 'about', component: AboutUsComponent },
     { path: 'contact', component: ContactusComponent },
+    { path : 'gallery', component: GalleryComponent}
 ];
