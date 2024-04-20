08d2e0d05e411256df0801ac8d4bdfb7c10a27ea
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-20 13:50:45 +0530

    about us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,14 @@ import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { ContactusComponent } from './pages/contactus/contactus.component';
 import { HomeComponent } from './pages/home/home.component';
 import { GalleryComponent } from './pages/gallery/gallery.component';
+import { NewsComponent } from './pages/news/news.component';
 
 export const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     {path : 'home', component : HomeComponent},
     { path: 'about', component: AboutUsComponent },
     { path: 'contact', component: ContactusComponent },
-    { path : 'gallery', component: GalleryComponent}
+    { path : 'gallery', component: GalleryComponent},
+    {path : 'news', component: NewsComponent}
+
 ];
