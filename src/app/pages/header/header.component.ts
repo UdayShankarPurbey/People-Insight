5a69e2a5dba9ccd989ba0b38eade709a39dd70e1
UdayShankarPurbey <chandanpurbey754@gmail.com>
2024-04-20 11:39:18 +0530

    apply tailwind in header and change it completely

--- src/app/pages/header/header.component.ts
+++ src/app/pages/header/header.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   standalone: true,
   imports: [
-    RouterLink
+    RouterLink,
+    RouterLinkActive
   ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
